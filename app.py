274008c3c43d4dc3ed16ea391155a586fdeba42e
NavyaSriMutthavarapu <navyasri0305@gmail.com>
2026-02-18 23:32:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pickle
+from text_vectorization import clean
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+# Streamlit UI
+st.title("🍽️ Review Sentiment Classifier")
+st.write("Enter a review to check if it is Positive or Negative")
+
+user_input = st.text_area("Enter Review")
+
+if st.button("Predict"):
+    if user_input.strip() != "":
+        cleaned = clean(user_input)
+        vectorized = vectorizer.transform([cleaned])
+        prediction = model.predict(vectorized)[0]
+
+        if prediction == 1:
+            st.success("✅ Positive Review")
+        else:
+            st.error("❌ Negative Review")
+    else:
+        st.warning("Please enter some text")
