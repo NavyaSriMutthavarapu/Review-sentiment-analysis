9e24fbfe52b59638bba06cab80869e0b7a7ae1d4
NavyaSriMutthavarapu <navyasri0305@gmail.com>
2026-02-18 23:59:23 +0530

    Update text_vectorization.py

--- text_vectorization.py
+++ text_vectorization.py
@@ -2,12 +2,9 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
 
 nltk.download('stopwords')
 nltk.download('wordnet')
-
 
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
@@ -15,13 +12,12 @@ lemmatizer = WordNetLemmatizer()
 def clean(doc):
     doc = doc.lower()
     doc = re.sub(r'[^a-zA-Z\s]', '', doc)
-    doc = re.sub(r'\s+', ' ', doc).strip()
+    doc = re.sub(r'\s+', ' ', doc)
 
-    tokens = word_tokenize(doc)
-    tokens = [word for word in tokens if word not in stop_words]
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    words = doc.split()
+    words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
 
-    return " ".join(tokens)
+    return " ".join(words)
 
 
 
